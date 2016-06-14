c35201c8e694cc05216f94f2214405bf336a3cff
Achal B I <achal.rvce@gmail.com>
2016-06-14 08:12:13 +0000

    removed unnecessary migration

--- db/migrate/20160613051749_add_answerable_to_survey_answer.rb
+++
@@ -1,5 +0,0 @@
-class AddAnswerableToSurveyAnswer < ActiveRecord::Migration
-  def change
-      add_index :survey_answers, [:survey_answerable_id, :survey_answerable_type], :unique => true, :name => "index_survey_answers_on_sa_type_and_sa_id"
-  end
-end
